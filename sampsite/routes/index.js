a3e7e3c8ca9b1001dce8acae7def1b085b4394eb
“John <johnjamesmartin@live.com>
2016-10-10 20:42:14 +0100

    Set up index.js and created studentlist.jade

--- sampsite/routes/index.js
+++ sampsite/routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
 
-/* GET home page. */
+
+/* Root route: */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+/* List route: */
+
+router.get('/thelist', function(req, res) {
+  var MongoClient = mongodb.MongoClient;
+  var url = 'mongod://localhost:27017/sampsite';
+
+  MongoClient.connect(url, function(err, data) {
+    if (err) {
+      console.log('Unable to connect to database: ' + err);
+    } else {
+      console.log('Connection established with: ' + url);
+      var collection = db.collection('students');
+      collection.find([]).toArray(function(err, results) {
+        if (err) {
+          res.send(err);
+        } else if (result.length) {
+          res.render('studentlist', {
+            'studentlist': result
+          });
+        } else {
+          res.send('No documents found');
+        }
+        db.close();
+      });
+    }
+  })
 });
 
 module.exports = router;
